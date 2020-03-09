4ba925acb22d30e6f941fd9c5b1ed3df7e38d06d
MatheusJCastro <matheusj_castro@icloud.com>
2020-03-09 00:04:44 -0300

    Version 6.0 - add Graphic Interface

--- README.md
+++ README.md
@@ -20,70 +20,53 @@ Todos os arquivos devem estar na mesma pasta em que o *medidas.py*.
 	Os dias devem estar no formado DDMM, entre aspas duplas (") e separados por vírgulas. Exemplo:
 	
 		dias = ["0503", "0603"]
-	
-* Modifique o `1` em `bias=1` e `lamp=1` para `0` caso não queria tirar algum dos tipos de ruído na linha:
-	
-		reduced = noise_removal(dt, noi_bias, noi_lamp, bias=1, lamp=1)
 		
-	O ruído `bias` se refere a medida tomada com a fenda da fotocélula
-totalmente tampada. Já o `lamp` se refere a medida tomada com a lampada desligada.
-		
-### Salvar dados sem ruído em *.csv*
-
-* Para salvar todos os dados sem o ruído no formato csv, descomente a linha:
-
-		save_new_data(reduced)
-		
-	O programa criará uma pasta com o nome `reduced_data` e salvará todos os arquivo individualmente no mesmo formato dos arquivos de entrada, acrescentando apenas *"reduced"* no final de cada arquivo.  
-	
-	>**Atenção:** Cada vez que executado, o programa sobrescreve os arquivos que contêm o mesmo nome.
-
-### Plotar e Salvar Gráficos
-
-* Descomente essa linha para plotar todos os gráficos de frequência:
-
-		plot_all(reduced, results_1, save=False, show_meth_1=False)
-
-	Troque `False` para `True` em `save=False` caso queira salvar uma imagem png do gráfico.
-	
-	![](Plot_of_all_data_example.png)
-	
-* Descomente essa linha  plotar um gráfico de uma intensidade k (todas frequências):
-		
-		plot_same_intensity(reduced, results_1, save=False, k=100, show_meth_1=False)
-
-	Troque `False` para `True` em `save=False` caso queira salvar uma imagem png do gráfico.
-	
-	![](Plot_same_intensity_example.png)
-	
-
 ## Execução
-Para rodar o código é necessario ter *Python3*, *Numpy* e *Matplotlib* instalados.  
+Para rodar o código é necessario ter *Python3*, *Tkinter*, *Numpy* e *Matplotlib* instalados.  
 Em sistemas linux rode o comando no terminal para executar o programa:
 	
 	python3 medidas.py
 
-## Método 1
-O método 1 consiste em pegar o ponto mais próximo de 0 e atribuir esse valor a V0. Para obter o resultado do método 1 para todos os gráficos, descomente a linha:
+Irá aparecer uma janela de pop-up para poder configurar e executar o programa.
 
-	results_1 = method_1(reduced, save=False)
+![](window.png)
+
+## Remoção do Ruído
 	
-Para salvar os resultados em um arquivo *.csv*, troque `False` para `True`. O programa cria a pasta `reduced_data` e salva o arquivo com nome "*1st_method_results.csv*" dentro dessa pasta.  
-Há a possibilidade de plotar os resultados nos gráficos, para isso troque o `False` para `True` em `show_meth_1=False` nas linhas que habilitam o plot dos gráficos, conforme pode ser observado na seção **Plotar e Salvar Gráficos**.
+No primeiro campo, selecione os ruídos que deseja retirar. As opções são `Bias` e `Lamp`. Caso queira salvar todos os dados, marque a caixa de seleção `Salvar`. Depois clique em `Remover ruído`.  
 
-> Para que o plot dos dados do método 1 funcione, é necessário que a linha que executa o método 1 esteja descomentada.
+> **Atenção:** Mesmo que não vá remover nenhum ruído, é necessario clicar em `Remover ruído`.
+
+Se escolher salvar os novos dados, o programa criará uma pasta com o nome `reduced_data` e salvará todos os arquivo individualmente no mesmo formato dos arquivos de entrada, acrescentando apenas *"reduced"* no final de cada arquivo.  
+	
+>**Atenção:** Cada vez que executado, o programa sobrescreve os arquivos que contêm o mesmo nome.
+		
+O ruído `Bias` se refere a medida tomada com a fenda da fotocélula totalmente tampada. Já o `Lamp` se refere a medida tomada com a lampada desligada.
+
+## Método 1
+O método 1 consiste em pegar o ponto mais próximo de 0 e atribuir esse valor a V0.  
+Para obter o resultado do método 1 para todos os gráficos, maruqe a caixa `1` em `Selecione os métodos que deseja calcular:`. Para salvar os resultados em um arquivo *.csv* marque a caixa de seleção do lado do botão `Calcular`. O programa cria a pasta `reduced_data` e salva o arquivo com nome "*1st_method_results.csv*" dentro dessa pasta.  
+Há a possibilidade de plotar os resultados nos gráficos, para isso siga as instruções na seção **Plotar e Salvar Gráficos**.
 
 ## Método 3
 O método 3 consiste em pegar a interseção das cruvas de intensidade de uma determinada frequência. Para isso o programa pega o valor em Volts da interseção de duas retas, dando como resultado a mediana dos valores.  
-Para executar o método 3, descomente a linha:
+Para executar o método 3, maruqe a caixa `3` em `Selecione os métodos que deseja calcular:`. Para salvar os resultados em um arquivo *.csv* marque a caixa de seleção do lado do botão `Calcular`. O programa cria a pasta `reduced_data` e salva o arquivo com nome "*3st_method_results.csv*" dentro dessa pasta.  
+Há a possibilidade de plotar os resultados nos gráficos, para isso siga as instruções na seção **Plotar e Salvar Gráficos**. 
 
-	results_2 = method_3(reduced, save=False)
+### Plotar e Salvar Gráficos
+
+* Para plotar todos os gráficos de frequência, selecione `Mostrar` e depois em `Gerar`. Para salvar, selecione tambám `Salvar`. Um exemplo do gráfico gerado pode ser observado na imagem a seguir:
 	
-Para salvar os resultados em um arquivo *.csv*, troque `False` para `True`. O programa cria a pasta `reduced_data` e salva o arquivo com nome "*3st_method_results.csv*" dentro dessa pasta.  
-Há a possibilidade de plotar os resultados nos gráficos, para isso troque o `False` para `True` em `show_meth_3=False` nas linhas que habilitam o plot dos gráficos, conforme pode ser observado na seção **Plotar e Salvar Gráficos**.
-
-> Para que o plot dos dados do método 1 funcione, é necessário que a linha que executa o método 1 esteja descomentada.
-
+	![](Plot_of_all_data_example.png)
+	
+* Para plotar um gráfico de uma intensidade k (todas frequências), selecione a intensidade desejada ao lado de `Escolha uma intensidade:`, selecione a caixa `Mostrar` e clique em `Gerar`. Para salvar, selecione tambám `Salvar`. Abaixo, um exemplo do gráfico gerado:
+	
+	![](Plot_same_intensity_example.png)
+	
 ## Exemplo de Gráficos com os métodos 1 e 3 aplicados
+Para plotar os gráficos mostrando os métodos que desejar, selecione as caixas abaixo de `Mostrar resultado dos métodos:`. e depois siga os mesmos procedimentos descritos em **Plotar e Salvar Gráficos**.  
+Abaixo segue mais dois exemplos dos gráficos gerados:
+	
 ![](Plot_of_all_data_dots_example.png)
+
 ![](Plot_same_intensity_dots_example.png)
--- medida.py
+++ medida.py
@@ -1,20 +1,27 @@
 ################################################################
 # Redução de Dados Física Experimental V - Efeito Fotoelétrico #
 # Matheus J. Castro                                            #
-# Version 5.2                                                  #
+# Version 6.0                                                  #
 # Last Modification: 8 de Março de 2020                        #
 ################################################################
 
 import numpy as np
 import os
 import matplotlib.pyplot as plt
+import tkinter as tk
+
+window = tk.Tk()
+width = 505
+height = 430
+posRight = int(window.winfo_screenwidth() / 2 - width / 2)
+posDown = int(window.winfo_screenheight() / 2 - height / 2)
+window.geometry("{}x{}+{}+{}".format(int(width), int(height), posRight, posDown))
+window.title("Efeito Fotoelétrico por Matheus J. Castro")
 
 cores = ["violeta", "azul", "verde", "amarelo", "vermelho"]
 intensidade = [100, 80, 60, 40, 20]
 dias = ["0503", "0603"]  # modifique essa lista colocando os dias que houve tomada de dados
-
-
-# apenas fomato DDMM entre aspas duplas
+                         # apenas fomato DDMM entre aspas duplas
 
 
 def input_data():
@@ -58,7 +65,7 @@ def noise_removal(final_data, noise_bias, noise_lamp, bias=1, lamp=1):
     return final_data
 
 
-def plot_all(data, meth_1, meth_3, save=False, show_meth_1=False, show_meth_3=False):
+def plot_all(data, meth_1, meth_3, show=True, save=False, show_meth_1=False, show_meth_3=False):
     color = {100: "black", 80: "blue", 60: "red", 40: "green", 20: "yellow"}
     if (len(cores) % 2) == 0:
         col = len(cores) / 2
@@ -94,10 +101,13 @@ def plot_all(data, meth_1, meth_3, save=False, show_meth_1=False, show_meth_3=Fa
 
     if save:
         plt.savefig("Plot_of_all_data")
-    plt.show()
+    if show:
+        plt.show()
+    else:
+        plt.close()
 
 
-def plot_same_intensity(data, meth_1, meth_3, k=100, save=False, show_meth_1=False, show_meth_3=False):
+def plot_same_intensity(data, meth_1, meth_3, k=100, show=True, save=False, show_meth_1=False, show_meth_3=False):
     color = {cores[0]: "violet", cores[1]: "blue", cores[2]: "green", cores[3]: "yellow", cores[4]: "red"}
 
     plt.figure(figsize=(8, 4.5))
@@ -124,7 +134,10 @@ def plot_same_intensity(data, meth_1, meth_3, k=100, save=False, show_meth_1=Fal
 
     if save:
         plt.savefig("Plot_same_intensity")
-    plt.show()
+    if show:
+        plt.show()
+    else:
+        plt.close()
 
 
 def save_new_data(data):
@@ -199,15 +212,229 @@ def method_3(data, save=False):
     return results
 
 
+def check():
+    global reduced_status
+    global calc_1, calc_2, calc_3, calc_4
+
+    reduced_status = False
+    calc_1 = False
+    calc_2 = False
+    calc_3 = False
+    calc_4 = False
+
+    verify_2.config(bg="red", text="Ruído não removido")
+    status_calc.config(bg="red", text="Calcular")
+
+    if (bias.get() == 1) & (lamp.get() == 0):
+        verify_1.config(text="Remover Bias")
+    elif (bias.get() == 0) & (lamp.get() == 1):
+        verify_1.config(text="Remover Lamp")
+    elif (bias.get() == 0) & (lamp.get() == 0):
+        verify_1.config(text="Não remover nada")
+    else:
+        verify_1.config(text="Remover Bias e Lamp")
+
+
+def reset():
+    global reduced_status, calc_1, calc_2, calc_3, calc_4
+
+    reduced_status = False
+    calc_1 = False
+    calc_2 = False
+    calc_3 = False
+    calc_4 = False
+
+    bias.set(0)
+    lamp.set(0)
+    save_ndata.set(0)
+    meth1_op.set(0)
+    meth2_op.set(0)
+    meth3_op.set(0)
+    meth4_op.set(0)
+    save_meths.set(False)
+    plot1_op.set(False)
+    save_plot1_op.set(False)
+    plot2_op.set(False)
+    save_plot2_op.set(False)
+    meth1_show.set(False)
+    meth2_show.set(False)
+    meth3_show.set(False)
+    meth4_show.set(False)
+    inten_op.set(100)
+
+    verify_1.config(bg='lightgray', text="Não remover nada")
+    verify_2.config(bg='red', text="Ruído não removido")
+    status_calc.config(text="Calcular", command=calc, bg="red")
+
+
+def remove():
+    global reduced, reduced_status
+
+    dt, noi_bias, noi_lamp = input_data()
+    reduced = noise_removal(dt, noi_bias, noi_lamp, bias=bias.get(), lamp=lamp.get())
+
+    if save_ndata.get():
+        save_new_data(reduced)
+
+    reduced_status = True
+    verify_2.config(bg='lightgreen', text="Ruído removido")
+    status_calc.config(bg="red", text="Calcular")
+
+
+def calc():
+    global reduced, results_1, results_3
+    global calc_1, calc_2, calc_3, calc_4
+
+    if reduced_status:
+        if meth1_op.get() == 1:
+            results_1 = method_1(reduced, save=save_meths.get())
+            calc_1 =True
+        if meth3_op.get() == 1:
+            results_3 = method_3(reduced, save=save_meths.get())
+            calc_3 = True
+
+        status_calc.config(bg="lightgreen", text="Calcular")
+
+        if meth1_op.get() + meth2_op.get() + meth3_op.get() + meth4_op.get() == 0:
+            status_calc.config(bg="red", text="Nenhum método selecionado")
+    else:
+        status_calc.config(bg="red", text="Aperte o \"botão Remover ruído\"")
+
+
+def plot_all_menu():
+    global reduced, reduced_status, results_1, results_3
+
+    show1 = False
+    show2 = False
+    show3 = False
+    show4 = False
+
+    if meth1_show.get() and calc_1:
+        show1 = True
+    if meth2_show.get() and calc_2:
+        show2 = True
+    if meth3_show.get() and calc_3:
+        show3 = True
+    if meth4_show.get() and calc_4:
+        show4 = True
+
+    if reduced_status:
+        plot_all(reduced, results_1, results_3, show=plot1_op.get(), save=save_plot1_op.get(),
+                 show_meth_1=show1, show_meth_3=show3)
+
+
+def plot_same_menu():
+    global reduced, reduced_status, results_1, results_3
+
+    show1 = False
+    show2 = False
+    show3 = False
+    show4 = False
+
+    if meth1_show.get() and calc_1:
+        show1 = True
+    if meth2_show.get() and calc_2:
+        show2 = True
+    if meth3_show.get() and calc_3:
+        show3 = True
+    if meth4_show.get() and calc_4:
+        show4 = True
+
+    if reduced_status:
+        plot_same_intensity(reduced, results_1, results_3, show=plot2_op.get(), save=False, k=inten_op.get(), show_meth_1=show1, show_meth_3=show3)
+
+
+def main():
+    ######################################################################################
+    # Remove Noise menu
+    tk.Label(window, text="Selecione os ruídos que deseja remover e clique em \"Remover ruído\".").grid(row=0, column=0, columnspan=4, sticky=tk.W+tk.E+tk.N+tk.S)
+    bias_button = tk.Checkbutton(window, text="Bias", variable=bias, onvalue=1, offvalue=0, command=check)
+    bias_button.grid(row=1, column=0, sticky=tk.W + tk.E + tk.N + tk.S)
+    lamp_button = tk.Checkbutton(window, text="Lamp", variable=lamp, onvalue=1, offvalue=0, command=check)
+    lamp_button.grid(row=1, column=1, sticky=tk.W + tk.E + tk.N + tk.S)
+    verify_1.grid(row=1, column=2, columnspan=2, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Button(window, text="Remover ruído", command=remove).grid(row=2, column=0, columnspan=2, sticky=tk.W+tk.E+tk.N+tk.S)
+    save_button = tk.Checkbutton(window, text="Salvar", variable=save_ndata, onvalue=True, offvalue=False)
+    save_button.grid(row=2, column=2, sticky=tk.W + tk.E + tk.N + tk.S)
+    verify_2.grid(row=2, column=3, columnspan=1, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Label(window, text="{}".format("-"*100)).grid(row=3, column=0, columnspan=4, sticky=tk.W + tk.E + tk.N + tk.S)
+    ######################################################################################
+    # Methods menu
+    tk.Label(window, text="Selecione os métodos que deseja calcular:").grid(row=4, column=0, columnspan=4, sticky=tk.W + tk.E + tk.N + tk.S)
+    meth1_button = tk.Checkbutton(window, text="1", variable=meth1_op, onvalue=1, offvalue=0)
+    meth1_button.grid(row=5, column=0, sticky=tk.W + tk.E + tk.N + tk.S)
+    meth2_button = tk.Checkbutton(window, text="2", variable=meth2_op, onvalue=1, offvalue=0, state=tk.DISABLED)
+    meth2_button.grid(row=5, column=1, sticky=tk.W + tk.E + tk.N + tk.S)
+    meth3_button = tk.Checkbutton(window, text="3", variable=meth3_op, onvalue=1, offvalue=0)
+    meth3_button.grid(row=5, column=2, sticky=tk.W + tk.E + tk.N + tk.S)
+    meth4_button = tk.Checkbutton(window, text="4", variable=meth4_op, onvalue=1, offvalue=0, state=tk.DISABLED)
+    meth4_button.grid(row=5, column=3, sticky=tk.W + tk.E + tk.N + tk.S)
+    status_calc.grid(row=6, column=0, columnspan=3, sticky=tk.W + tk.E + tk.N + tk.S)
+    save_meths_button = tk.Checkbutton(window, text="Salvar", variable=save_meths, onvalue=True, offvalue=False)
+    save_meths_button.grid(row=6, column=3, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Label(window, text="{}".format("-" * 100)).grid(row=7, column=0, columnspan=4, sticky=tk.W + tk.E + tk.N + tk.S)
+    ######################################################################################
+    # Plots menu
+    tk.Label(window, text="Plotar e salvar gráficos\nMostrar resultado dos métodos:").grid(row=8, column=0, columnspan=4, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Checkbutton(window, text="1", variable=meth1_show, onvalue=True, offvalue=False).grid(row=9, column=0, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Checkbutton(window, text="2", variable=meth2_show, onvalue=True, offvalue=False, state=tk.DISABLED).grid(row=9, column=1, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Checkbutton(window, text="3", variable=meth3_show, onvalue=True, offvalue=False).grid(row=9, column=2, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Checkbutton(window, text="4", variable=meth4_show, onvalue=True, offvalue=False, state=tk.DISABLED).grid(row=9, column=3, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Label(window, text="{}".format("- " * 50)).grid(row=10, column=0, columnspan=4, sticky=tk.W + tk.E + tk.N + tk.S)
+    #######################################
+    tk.Label(window, text="Gráficos de todas\nas frequências:").grid(row=11, column=0, columnspan=1, sticky=tk.W + tk.E + tk.N + tk.S)
+    plot1_button = tk.Checkbutton(window, text="Mostrar", variable=plot1_op, onvalue=True, offvalue=False)
+    plot1_button.grid(row=11, column=1, sticky=tk.W + tk.E + tk.N + tk.S)
+    save_plot1_button = tk.Checkbutton(window, text="Salvar", variable=save_plot1_op, onvalue=True, offvalue=False)
+    save_plot1_button.grid(row=11, column=2, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Button(window, text="Gerar", command=plot_all_menu).grid(row=11, column=3, columnspan=1, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Label(window, text="{}".format("- " * 50)).grid(row=12, column=0, columnspan=4, sticky=tk.W + tk.E + tk.N + tk.S)
+    #######################################
+    tk.Label(window, text="Gráfico de uma\nintensidade:").grid(row=13, column=0, columnspan=1, rowspan=2, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Label(window, text="Escolha uma intensidade:").grid(row=13, column=1, columnspan=2, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.OptionMenu(window, inten_op, *[100, 80, 60, 40, 20]).grid(row=13, column=3, columnspan=1, sticky=tk.W + tk.E + tk.N + tk.S)
+    plot1_button = tk.Checkbutton(window, text="Mostrar", variable=plot2_op, onvalue=True, offvalue=False)
+    plot1_button.grid(row=14, column=1, sticky=tk.W + tk.E + tk.N + tk.S)
+    save_plot1_button = tk.Checkbutton(window, text="Salvar", variable=save_plot2_op, onvalue=True, offvalue=False)
+    save_plot1_button.grid(row=14, column=2, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Button(window, text="Gerar", command=plot_same_menu).grid(row=14, column=3, columnspan=1, sticky=tk.W + tk.E + tk.N + tk.S)
+    ######################################################################################
+    # End menu
+    tk.Button(window, text="Reset", command=reset).grid(row=15, column=0, columnspan=2, sticky=tk.W + tk.E + tk.N + tk.S)
+    tk.Button(window, text="Quit", command=window.quit).grid(row=15, column=2, columnspan=2, sticky=tk.W + tk.E + tk.N + tk.S)
+    window.mainloop()
+
+
 results_1 = {}
-results_2 = {}
-dt, noi_bias, noi_lamp = input_data()
-reduced = noise_removal(dt, noi_bias, noi_lamp, bias=1, lamp=0)  # troque de 1 para 0 caso não vá remover algum dos
-                                                                 # tipos de ruido
-# save_new_data(reduced)  # descomente essa linha para salvar os dados no formato .csv
-#results_1 = method_1(reduced, save=False)  # não está pronto
-#results_2 = method_3(reduced, save=False)
-#plot_all(reduced, results_1, results_2, save=False, show_meth_1=False, show_meth_3=False)  # descomente essa linha para plotar todos os dados
-#plot_same_intensity(reduced, results_1, results_2, save=False, k=100, show_meth_1=False, show_meth_3=False)   # descomente essa linha para plotar um grafico com todas as
-                                                                                            # frequencias em uma determinada intensidade (k)
-                                                                                            # caso deseje salvar qualquer plot, troque False para True
+results_3 = {}
+reduced = {}
+reduced_status = False
+calc_1 = False
+calc_2 = False
+calc_3 = False
+calc_4 = False
+
+bias = tk.IntVar()
+lamp = tk.IntVar()
+save_ndata = tk.BooleanVar()
+meth1_op = tk.IntVar()
+meth2_op = tk.IntVar()
+meth3_op = tk.IntVar()
+meth4_op = tk.IntVar()
+save_meths = tk.BooleanVar()
+plot1_op = tk.BooleanVar()
+save_plot1_op = tk.BooleanVar()
+plot2_op = tk.BooleanVar()
+save_plot2_op = tk.BooleanVar()
+meth1_show = tk.BooleanVar()
+meth2_show = tk.BooleanVar()
+meth3_show = tk.BooleanVar()
+meth4_show = tk.BooleanVar()
+inten_op = tk.IntVar()
+inten_op.set(100)
+
+verify_1 = tk.Label(window, bg='lightgray', text="Não remover nada")
+verify_2 = tk.Label(window, bg='red', text="Ruído não removido")
+status_calc = tk.Button(window, text="Calcular", command=calc, bg="red")
+
+main()
